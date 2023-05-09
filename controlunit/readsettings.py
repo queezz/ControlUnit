41fee409be28fdd39d43e4d6c0a4e17cb6e12af2
Arseniy <queezz@gmail.com>
2023-05-09 17:17:10 +0900

    datafolder starts with ~: put folder in home dir

--- controlunit/readsettings.py
+++ controlunit/readsettings.py
@@ -1,41 +1,52 @@
 import csv
 import os
-from os.path import join
+from os.path import join, expanduser
+
 
 def make_datafolders():
-    """ Create folder for saving data, if not existing """ 
+    """
+    Create folder for saving data, if not existing
+    if datafolder starts with '~' - put the folder in home directory
+    """
     settings = read_settings()
-    foldername = settings['datafolder']
+    foldername = settings["datafolder"]
+
+    if foldername.startswith("~"):
+        home = expanduser("~")
+        foldername = home + foldername[1:]
 
     try:
         os.makedirs(foldername)
-        print(f'created {foldername}')
+        print(f"created {foldername}")
     except FileExistsError:
-        pass 
+        pass
+
 
 def read_settings():
     """ Read .settings and get datafoldr path"""
     pth = None
     sampling_rate = 0.01
-    with open('.settings', 'r') as f:
-        s = csv.reader(f, delimiter=',')
+    with open(".settings", "r") as f:
+        s = csv.reader(f, delimiter=",")
         for r in s:
-            if r[0] == 'datafolder':
+            if r[0] == "datafolder":
                 pth = r[1].strip()
-            if r[0] == 'pathislocal':
+            if r[0] == "pathislocal":
                 local = r[1].strip()
-            if r[0] == 'sampling_rate':
-                sampling_rate = r[1].strip() 
-    if local == 'True': local = True
+            if r[0] == "sampling_rate":
+                sampling_rate = r[1].strip()
+    if local == "True":
+        local = True
     sampling_rate = float(sampling_rate)
     settings = {
-        'datafolder':pth,
-        'pathislocal':local,
-        'samplingtime':sampling_rate,
+        "datafolder": pth,
+        "pathislocal": local,
+        "samplingtime": sampling_rate,
     }
 
     return settings
 
-if __name__ == '__main__':
-    #print(read_settings())
+
+if __name__ == "__main__":
+    # print(read_settings())
     make_datafolders()
